73b42df5da9da4943a7631dde10ce12a4164fc28
Izulia <florian.bernou@gmail.com>
2016-11-09 10:03:59 +0100

    Initial Commit

---
+++ Can U Do It/Assets/Scripts/Pause.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+    bool showGUI = false;
+    GameObject canvas;
+
+	// Use this for initialization
+	void Start () {
+        canvas = GameObject.Find("Canvas");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            showGUI = !showGUI;
+        }
+
+        if (showGUI == true)
+        {
+            canvas.SetActive(true);
+        }
+        else
+        {
+            canvas.SetActive(false);
+        }
+    }
+}
---
+++ Can U Do It/Assets/Scripts/Script.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function BUTTON_START () {
+    Application.LoadLevel("Niveaux/niveau1");
+}
+
+function BUTTON_LEAVE () {
+    Application.Quit();
+}
+
+function BUTTON_MENU () {
+    Application.LoadLevel("Menu");
+}
